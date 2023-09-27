c689cc7f13b2ba9a1e55d188f1a634977301fda8
hieu pham <phamduchieu0310@gmail.com>
2023-09-27 17:33:29 +0700

    Add auto increment for id

--- migrations/versions/d2016f232654_add_user_table.py
+++ migrations/versions/d2016f232654_add_user_table.py
@@ -22,7 +22,7 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
     op.create_table(
         'users',
-        sa.Column('id', sa.Integer, primary_key=True), 
+        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True), 
         sa.Column('phone_number', sa.String(10), nullable=False),
         sa.Column('facebook_account', sa.String(50), nullable=True),
         sa.Column('google_account', sa.String(50), nullable=True),
